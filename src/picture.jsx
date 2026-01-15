3969af0f0a10d2dd691d5b90437c69513c1e37a9
Isa Robertini <robertiniisa@gmail.com>
2026-01-15 17:03:13 +0100

    updated toml and dist

--- src/picture.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export const Picture = () => {
-    return (
-        <img className="h-96" src="https://img.freepik.com/free-vector/bokeh-theme-wallpaper_23-2148467962.jpg?semt=ais_hybrid&w=740&q=80" alt="" />
-    )
-}
